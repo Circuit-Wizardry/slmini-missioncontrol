6e4a7cf4dfd562f210c6deb64020f0fe2883c719
Witherflare <80921641+Witherflare@users.noreply.github.com>
2024-04-06 17:34:47 -0400

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,9 @@
+mpremote fs cp main.py :main.py
+mpremote fs cp starlight_mini.py :starlight_mini.py
+mpremote fs cp starlight.py :starlight.py
+mpremote fs cp gpio.py :gpio.py
+mpremote fs cp data.json :data.json
+mpremote fs cp fusion.py :fusion.py
+mpremote fs cp deltat.py :deltat.py
+
+mpremote run main.py
